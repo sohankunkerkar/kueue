b3c7925c1ab77ee5fe11164ffc47142ea28ffa5c
Sohan Kunkerkar <sohank2602@gmail.com>
2025-02-20 09:13:12 -0500

    test-2

--- Makefile-test-ocp.mk
+++ Makefile-test-ocp.mk
@@ -1,7 +1,3 @@
-include Makefile
-include Makefile-deps.mk
-include Makefile-test.mk
-
 PROJECT_DIR := $(shell dirname $(abspath $(lastword $(MAKEFILE_LIST))))
 ARTIFACTS ?= $(PROJECT_DIR)/bin
 
@@ -21,8 +17,28 @@ version_pkg = sigs.k8s.io/kueue/pkg/version
 LD_FLAGS += -X '$(version_pkg).GitVersion=$(GIT_TAG)'
 LD_FLAGS += -X '$(version_pkg).GitCommit=$(shell git rev-parse HEAD)'
 
+# Use go.mod go version as source.
+GINKGO_VERSION ?= $(shell $(GO_CMD) list -m -f '{{.Version}}' github.com/onsi/ginkgo/v2)
+KUSTOMIZE_VERSION ?= $(shell $(GO_CMD) list -m -f '{{.Version}}' sigs.k8s.io/kustomize/kustomize/v5)
+YQ_VERSION ?= $(shell $(GO_CMD) list -m -f '{{.Version}}' github.com/mikefarah/yq/v4)
+
+KUSTOMIZE = $(PROJECT_DIR)/bin/kustomize
+.PHONY: kustomize
+kustomize: ## Download kustomize locally if necessary.
+	@GOBIN=$(PROJECT_DIR)/bin GO111MODULE=on $(GO_CMD) install sigs.k8s.io/kustomize/kustomize/v5@$(KUSTOMIZE_VERSION)
+
+GINKGO = $(PROJECT_DIR)/bin/ginkgo
+.PHONY: ginkgo
+ginkgo: ## Download ginkgo locally if necessary.
+	@GOBIN=$(PROJECT_DIR)/bin GO111MODULE=on $(GO_CMD) install github.com/onsi/ginkgo/v2/ginkgo@$(GINKGO_VERSION)
+
+YQ = $(PROJECT_DIR)/bin/yq
+.PHONY: yq
+yq: ## Download yq locally if necessary.
+	@GOBIN=$(PROJECT_DIR)/bin GO111MODULE=on $(GO_CMD) install github.com/mikefarah/yq/v4@$(YQ_VERSION)
+
 .PHONY: test-e2e-ocp
-test-e2e-ocp: gomod-download dep-crds kueuectl ginkgo-top run-test-e2e-ocp-singlecluster
+test-e2e-ocp: kustomize ginkgo yq dep-crds kueuectl ginkgo-top-ocp run-test-e2e-ocp-singlecluster
 run-test-e2e-ocp-singlecluster:
 	@echo "Running e2e tests on OpenShift cluster ($(shell oc whoami --show-server))"
 		ARTIFACTS="$(ARTIFACTS)/$@" IMAGE_TAG=$(IMAGE_TAG) GINKGO_ARGS="$(GINKGO_ARGS)" \
@@ -30,3 +46,10 @@ run-test-e2e-ocp-singlecluster:
 		./hack/e2e-test-ocp.sh
 	$(PROJECT_DIR)/bin/ginkgo-top -i $(ARTIFACTS)/$@/e2e.json > $(ARTIFACTS)/$@/e2e-top.yaml
 
+.PHONY: ginkgo-top-ocp
+ginkgo-top-ocp:
+	$(GO_BUILD_ENV) $(GO_CMD) build -ldflags="$(LD_FLAGS)" -o $(PROJECT_DIR)/bin/ginkgo-top ./ginkgo-top
+
+.PHONY: kueuectl
+kueuectl:
+	CGO_ENABLED=$(CGO_ENABLED) $(GO_BUILD_ENV) $(GO_CMD) build -ldflags="$(LD_FLAGS)" -o $(PROJECT_DIR)/bin/kubectl-kueue cmd/kueuectl/main.go
